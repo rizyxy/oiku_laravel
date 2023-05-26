5699ffb1130574755a18dd63b0fb732680b5c408
AirlanggaDS <airlangga.dwi.satrio-2021@fst.unair.ac.id>
2023-05-26 15:34:01 +0700

    add icon  footer

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="{!! asset('assets/css/app.css') !!}">
 </head>
 <body>
